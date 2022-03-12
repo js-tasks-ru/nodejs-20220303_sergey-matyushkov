78f5edf109b2ad969a0dc68c9d233ef60b55e204
Sergey Matyushkov <syumatyushkov@avito.ru>
2022-03-12 21:39:29 +0300

    add colution for 3d task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
-const url = require('url');
+// const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,12 +9,31 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (/\//.test(pathname)) {
+    res.statusCode = 400;
+    res.end('Not support nested folders');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
+  if (!fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end('Not found');
+    return;
+  }
 
+  switch (req.method) {
+    case 'GET': {
+      const fileStream = fs.createReadStream(filepath);
+      fileStream
+          .pipe(res)
+          .on('error', () => {
+            res.statusCode = 500;
+            res.end('Internal server error');
+          });
       break;
+    }
 
     default:
       res.statusCode = 501;
