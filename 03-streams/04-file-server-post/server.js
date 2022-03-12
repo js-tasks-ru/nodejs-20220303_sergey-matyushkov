5e2ade3076afbebcbda4f3b28becb113e2637faf
Sergey Matyushkov <syumatyushkov@avito.ru>
2022-03-12 23:33:56 +0300

    add solution for 4d task

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,7 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -8,12 +9,55 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (/\//.test(pathname)) {
+    res.statusCode = 400;
+    res.end('Not support nested folders');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (fs.existsSync(filepath)) {
+    res.statusCode = 409;
+    res.end('File exists');
+    return;
+  }
+
   switch (req.method) {
-    case 'POST':
+    case 'POST': {
+      const limitStream = new LimitSizeStream({
+        limit: 1048576,
+      });
+      const fileStream = fs.createWriteStream(filepath);
+
+      req.pipe(limitStream).pipe(fileStream);
+
+      req.on('aborted', () => {
+        fs.unlinkSync(filepath);
+        limitStream.destroy();
+        fileStream.destroy();
+      });
+
+      limitStream.on('error', () => {
+        res.statusCode = 413;
+        res.end('File is more than 1Mb');
+        fileStream.destroy();
+        fs.unlinkSync(filepath);
+      });
+
+      fileStream.on('error', () => {
+        fs.unlinkSync(filepath);
+        res.statusCode = 500;
+        res.end('Internal server error');
+      });
+
+      fileStream.on('finish', () => {
+        res.statusCode = 201;
+        res.end('File uploaded');
+      });
 
       break;
+    }
 
     default:
       res.statusCode = 501;
