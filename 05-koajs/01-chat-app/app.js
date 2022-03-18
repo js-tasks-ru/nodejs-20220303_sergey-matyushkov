eb47ff4eff976df8881936cf576983a6faf3d8a7
Sergey Matyushkov <syumatyushkov@avito.ru>
2022-03-18 21:47:41 +0300

    add colution for 5 task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,28 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let conections = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((resolve, _) => {
+    conections.push(resolve);
+  });
+
+  const message = await promise;
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (!message) {
+    return;
+  }
+
+  conections.forEach((resolve) => resolve(message));
+  conections = [];
+
+  ctx.body = 'message published';
 });
 
 app.use(router.routes());
