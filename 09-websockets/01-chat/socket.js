a46036d1235b6bdbb9bed66bc6aaba4bd3a628fe
Sergey Matyushkov <syumatyushkov@avito.ru>
2022-04-05 20:55:48 +0300

    add sol for 9

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -7,11 +7,31 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const {token} = socket.handshake.query;
+    if (!token) {
+      const error = new Error('anonymous sessions are not allowed');
+      return next(error);
+    }
+
+    const session = await Session.findOne({token}).populate('user');
+    if (!session) {
+      const error = new Error('wrong or expired session token');
+      return next(error);
+    }
+
+    socket.user = session.user;
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      await Message.create({
+        date: new Date(),
+        text: msg,
+        chat: socket.user._id,
+        user: socket.user.displayName,
+      });
+    });
   });
 
   return io;
